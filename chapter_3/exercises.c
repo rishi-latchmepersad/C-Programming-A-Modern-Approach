239e94d39cb34200f06c5fdd2dc56dd94f53b5b7
Rishi Latchmepersad <latchmepersad@gmail.com>
2025-12-31 06:56:20 -0400

    chapter 3 questions

---
+++ chapter_3/exercises.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    // question 1
+    printf("%6d,%4d\n", 86, 1040);
+    printf("%12.5e\n", 30.253);
+    printf("%.4f\n", 83.162);
+    printf("%-6.2g\n", .0000009979);
+    // question 2
+    float x = 1231342.3413414141f;
+    printf("%-8.1e\n", x);
+    printf("%10.6e\n", x);
+    printf("%-8.3f\n", x);
+    printf("%6.0f\n", x);
+    // question 3
+    // for (a), both are equivalent
+    // for (b), both are not equivalent
+    // for (c), both are not equivalent
+    // for (d), both are equivalent
+    //question 4
+    // i will be 10, x will be 0.3 and j will be 5
+    // question 5
+    // x will be 12.3, i will be 45, and y will be 0.6
+}
---
+++ chapter_3/programming_projects.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // // question 1
+    // printf("Please enter a date in the form mm/dd/yyyy: ");
+    // int month, day, year;
+    // scanf("%d/%d/%d", &month, &day, &year);
+    // printf("You entered the date %0004d%02d%02d\n", year, month, day);
+    // // question 2
+    // printf("Enter item number: ");
+    // int item_number;
+    // scanf("%d", &item_number);
+    // printf("Enter unit price: ");
+    // float unit_price;
+    // scanf("%f", &unit_price);
+    // printf("Enter purchase date (mm/dd/yyyy): ");
+    // int month, day, year;
+    // scanf("%d/%d/%d", &month, &day, &year);
+    // printf("Item\t\tUnit\t\tPurchase\n");
+    // printf("\t\tPrice\t\tDate\n");
+    // printf("%-8d\t$%7.2f\t%02d/%02d/%04d\n", item_number, unit_price, month, day, year);
+    // // question 3
+    // printf("Enter ISBN: ");
+    // int gsi_prefix, group_identifier, publisher_code, item_number, check_digit;
+    // scanf("%d-%d-%d-%d-%d", &gsi_prefix, &group_identifier, &publisher_code, &item_number, &check_digit);
+    // printf("GS1 prefix: %d\n", gsi_prefix);
+    // printf("Group identifier: %d\n", group_identifier);
+    // printf("Publisher code: %d\n", publisher_code);
+    // printf("Item number: %d\n", item_number);
+    // printf("Check digit: %d\n", check_digit);
+    // // question 4
+    // printf("Enter phone number [(xxx) xxx-xxxx]: ");
+    // int area_code, exchange, line_number;
+    // scanf("(%d) %d-%d", &area_code, &exchange, &line_number);
+    // printf("You entered %d.%d.%d\n", area_code, exchange, line_number);
+    // return 0;
+    // // question 5
+    // printf("Enter the numbers from 1 to 16 in any order: ");
+    // int n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13, n14, n15, n16;
+    // scanf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d",
+    //       &n1, &n2, &n3, &n4, &n5, &n6, &n7, &n8,
+    //       &n9, &n10, &n11, &n12, &n13, &n14, &n15, &n16);
+    // int row1_sum = n1 + n2 + n3 + n4;
+    // int row2_sum = n5 + n6 + n7 + n8;
+    // int row3_sum = n9 + n10 + n11 + n12;
+    // int row4_sum = n13 + n14 + n15 + n16;
+    // int col1_sum = n1 + n5 + n9 + n13;
+    // int col2_sum = n2 + n6 + n10 + n14;
+    // int col3_sum = n3 + n7 + n11 + n15;
+    // int col4_sum = n4 + n8 + n12 + n16;
+    // int diag1_sum = n1 + n6 + n11 + n16;
+    // int diag2_sum = n4 + n7 + n10 + n13;
+    // printf("%d %d %d %d\n", n1, n2, n3, n4);
+    // printf("%d %d %d %d\n", n5, n6, n7, n8);
+    // printf("%d %d %d %d\n", n9, n10, n11, n12);
+    // printf("%d %d %d %d\n", n13, n14, n15, n16);
+    // printf("Row sums: %d %d %d %d\n", row1_sum, row2_sum, row3_sum, row4_sum);
+    // printf("Column sums: %d %d %d %d\n", col1_sum, col2_sum, col3_sum, col4_sum);
+    // printf("Diagonal sums: %d %d\n", diag1_sum, diag2_sum);
+    // return 0;
+    // // question 6
+    // printf("Enter two fractions separated by a plus sign (a/b + c/d): ");
+    // int num1, denom1, num2, denom2;
+    // scanf("%d/%d + %d/%d", &num1, &denom1, &num2, &denom2);
+    // int sum_num = num1 * denom2 + num2 * denom1;
+    // int sum_denom = denom1 * denom2;
+    // printf("The sum is %d/%d\n", sum_num, sum_denom);
+    // return 0;
+}
